e893083011652ce55f67a0d6db7d6965a9b71a67
punitkumar11 <punit.kumar@rdpolytech.ca>
2024-11-18 09:46:05 -0700

    first commit

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//in the browser http://localhost:3000/user/23
+
 app.get("/user/:id", (req, res)=>{
     const userId = req.params.id;
-    res.send(User id is${userId});
+    res.send(`User id is`${userId});
 });
 
 app.listen(PORT, () => {
-    console.log(Server is running on http://localhost:${PORT});
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
